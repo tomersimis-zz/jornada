eea473e19e143ce7923d3bb455abf677e18b3a74
vddm <vddm@cin.ufpe.br>
2015-11-08 13:21:23 -0300

    Views para tela de estatística

--- jornada/views.py
+++ jornada/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='usuario/login/')
 def index(request):
 	return render(request, 'index.html')
+
+def statistics(request):
+	return render(request, 'reports.html')
